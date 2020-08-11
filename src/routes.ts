f5814ad8afc93e9ef0726cffa35da28f9d916ab8
Felipe <felipe.ferraz.eng@gmail.com>
2020-08-11 08:26:08 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import ClassesController from './controllers/ClassesController';
+import ConnectionController from './controllers/ConnectionsControllers';
+
+const routes = express.Router();
+
+const classesControllers = new ClassesController();
+const connectionsController = new ConnectionController
+
+
+
+routes.get('/classes', classesControllers.index);
+routes.post('/classes', classesControllers.create);
+
+routes.post('/connections', connectionsController.create);
+routes.get('/connections', connectionsController.index);
+
+export default routes;
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+console.log('Rodando 👌')
+
+
+app.listen(3333);
